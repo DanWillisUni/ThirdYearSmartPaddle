ca75aea8cfa31b7717e4e4b9155bba0e90592060
DanWillisUni <dan.willis.uni@gmail.com>
2022-04-01 15:56:50 +0100

    trying to turn off sensors on on button press

--- KayakDataCollection/app/src/main/java/com/example/kayakdatacollection/MainActivity.java
+++ KayakDataCollection/app/src/main/java/com/example/kayakdatacollection/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +19,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //setup dropdown
+        Spinner dropdown = findViewById(R.id.activity_spinner);
+        String[] items = new String[]{"Excellent", "Good", "Bad"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        //setup button
+        final boolean[] isInSession = {false};
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(isInSession[0]){
+                    isInSession[0] = false;
+                    button.setText("Start");
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    onPause();
+                }
+                else{
+                    isInSession[0] = true;
+                    button.setText("Stop");
+                    onResume();
+                }
+            }
+        });
+
+        //setup sensors
         accelerometer = new Accelerometer(this);
         gyroscope = new Gyroscope(this);
-
         accelerometer.setListener(new Accelerometer.Listener() {
             @Override
             public void onTranslation(float tx, float ty, float tz) {
@@ -29,14 +58,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         gyroscope.setListener(new Gyroscope.Listener() {
             @Override
             public void onRotation(float rx, float ry, float rz) {
-                if(rz > 1.0f){
+                if(ry > 1.0f){
                     getWindow().getDecorView().setBackgroundColor(Color.GREEN);
                 }
-                else if(rz < -1.0f){
+                else if(ry< -1.0f){
                     getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
                 }
             }
