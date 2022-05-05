aa9e8d04a49a4bd264eafd5de5f0fee13c2744cd
DanWillisUni <dan.willis.uni@gmail.com>
2022-05-05 17:27:56 +0100

    getting ready for new images

--- KayakDataCollection/app/src/main/java/com/example/kayakdatacollection/DisplayFeedback.java
+++ KayakDataCollection/app/src/main/java/com/example/kayakdatacollection/DisplayFeedback.java
@@ -38,6 +38,13 @@ public class DisplayFeedback extends AppCompatActivity {
         ImageView rank = (ImageView) findViewById(R.id.rankImage);
         rank.setImageResource(imageToDisplay);
 
+        ImageView correction = (ImageView) findViewById(R.id.correctionImage);
+        String biggestError = calculateBiggestError();
+        int correctionImage = getCorrectionImage(biggestError);
+        correction.setImageResource(correctionImage);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(getRankUpSentance(perfectPercent) + biggestError);
     }
 
     private double calculatePerfectPercent(){
@@ -49,15 +56,61 @@ public class DisplayFeedback extends AppCompatActivity {
     }
 
     private int getImageName(double perfectPercent){
-        /*if(perfectPercent < 20){
-
+        if(perfectPercent < 20){
+            return R.drawable.Tadpole;
         } else if(perfectPercent < 40){
-
+            return R.drawable.Salmon;
         } else if(perfectPercent < 60){
-
+            return R.drawable.Squid;
         } else if(perfectPercent < 80){
+            return R.drawable.Dolphin;
+        }
+        return R.drawable.Shark;
+    }
 
-        }*/
-        return R.drawable.shark;
+    private String getRankUpSentance(double perfectPercent){
+        if(perfectPercent < 20){
+            return "Like a Tadpole your journeys only just beginning \n" +
+                    "To become a salmon focus on ";
+        } else if(perfectPercent < 40){
+            return "Well done!  You are a salmon! \n" +
+                    "To become a squid, focus on ";
+        } else if(perfectPercent < 60){
+            return "Congratulations, you are a Salmon!\n" +
+                    " To become a Dolphin, focus on ";
+        } else if(perfectPercent < 80){
+            return "Great Job, you are a Dolphin! \n" +
+                    "To become a Shark focus on ";
+        }
+        String r = "Fantastic! You are a shark in the water!";
+        if (perfectPercent < 100){
+            r += "\nTo improve even more focus on ";
+        }
+        return r;
+    }
+
+    private String calculateBiggestError(){
+        int max = 0;
+        int maxI = 0;
+        for (int i = 1; i < feedbackArray.length; i++) {
+            if(feedbackArray[i]>max){
+                max = feedbackArray[i];
+                maxI = i;
+            }
+        }
+        return feedbackImageNames[maxI-1];
+    }
+
+    private int getCorrectionImage(String errorName){
+        if (errorName == "OverReaching"){
+            return R.drawable.OverReach;
+        } else if (errorName == "NotUpright"){
+            return R.drawable.Salmon;//change
+        } else if (errorName == "StrokeTooWide"){
+            return R.drawable.TooWide;
+        } else if (errorName == "BladeAngleWrong"){
+            return R.drawable.BladeAngle;
+        }
+        return R.drawable.Perfect;
     }
 }
